643dad4764abdde11cdcf319948fa516ec252ee2
Jeff <j.edjekoomhene@gmail.com>
2024-01-09 10:18:58 +0100

    clean up auth

--- credentials/AttioApi.credentials.ts
+++ credentials/AttioApi.credentials.ts
@@ -11,24 +11,14 @@ export class AttioApi implements ICredentialType {
 	documentationUrl = 'https://developers.attio.com/docs/making-your-first-api-call';
 	properties: INodeProperties[] = [
 		{
-			displayName: 'API Ky',
+			displayName: 'API Key',
 			name: 'apiKey',
 			type: 'string',
 			typeOptions: { password: true },
 			default: '',
 		},
-		{
-			displayName: 'Domain',
-			name: 'domain',
-			type: 'string',
-			default: 'https://api.attio.com/v2/objects',
-		},
 	];
 
-	// This allows the credential to be used by other parts of n8n
-	// stating how this credential is injected as part of the request
-	// An example is the Http Request node that can make generic calls
-	// reusing this credential
 	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
